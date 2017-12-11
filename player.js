faa96573a2a8382a6cb8306212fc53d9317143de
Jason Zheng <tobe.zheng.wei@gmail.com>
2017-12-11 17:14:47 +1100

    Update player.js

--- player.js
+++ player.js
@@ -1,33 +1,33 @@
-function Player(playerID) {  
+function Player(playerID) {
     this.id = playerID;
     this.name = "";
     this.tableID = "";
-    this.rounds=0;
+    this.rounds = 0;
     this.hand = [];
-    this.isZhuangJia=false
-    this.wins=0
+    this.isZhuangJia = false
+    this.wins = 0
     this.move = ""
-    this.isCardsShown=false
-    this.loss=0
-    this.drankCups=0
-    this.cardStatus={}
+    this.isCardsShown = false
+    this.loss = 0
+    this.drankCups = 0
+    this.cardStatus = {}
     this.status = "";
-   
+
 };
 
-Player.prototype.setName = function (name){
-	this.name=name
+Player.prototype.setName = function (name) {
+    this.name = name
 }
-Player.prototype.resetPlayer = function (){
-    this.rounds=0;
+Player.prototype.resetPlayer = function () {
+    this.rounds = 0;
     this.hand = [];
-    this.isZhuangJia=false
-    this.wins=0
-    this.gaiPai=false;
-    this.isCardsShown=false
-    this.loss=0
-    this.drankCups=0
-    this.cardStatus={}
+    this.isZhuangJia = false
+    this.wins = 0
+    this.gaiPai = false;
+    this.isCardsShown = false
+    this.loss = 0
+    this.drankCups = 0
+    this.cardStatus = {}
 }
 
 module.exports = Player;
