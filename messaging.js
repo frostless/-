df447d7bd961c82008fdc2ead66ca96cb5d2a7b5
Jason Zheng <tobe.zheng.wei@gmail.com>
2017-12-11 17:15:05 +1100

    Update messaging.js

--- messaging.js
+++ messaging.js
@@ -2,14 +2,14 @@ Game = require("./game.js");
 Table = require('./table.js');
 
 
-function Messaging() {};
+function Messaging() { };
 
-Messaging.prototype.createSampleTables = function(amount) {
+Messaging.prototype.createSampleTables = function (amount) {
 	var tableList = [];
-	for(var i = 0; i < amount; i++){
+	for (var i = 0; i < amount; i++) {
 		var game = new Game();
 		//var table = new Table(uuid.v4());
-		var table = new Table(i+1);
+		var table = new Table(i + 1);
 		table.setName("Test Table" + (i + 1));
 		table.gameObj = game;
 		table.pack = game.pack; //adds the shuffled pack from the constructor
@@ -19,48 +19,48 @@ Messaging.prototype.createSampleTables = function(amount) {
 	return tableList;
 };
 
-Messaging.prototype.sendEventToAllPlayers = function(event, message, io, players) {
-	for(var i = 0; i < players.length; i++){
+Messaging.prototype.sendEventToAllPlayers = function (event, message, io, players) {
+	for (var i = 0; i < players.length; i++) {
 		io.to(players[i].id).emit(event, message);
 	}
 };
-Messaging.prototype.sendEventToAllNotPlaying = function(event, message, io, players) {
-	for(var i = 0; i < players.length; i++){
-		if(players[i].status!=="intable"){
-		io.to(players[i].id).emit(event, message);
+Messaging.prototype.sendEventToAllNotPlaying = function (event, message, io, players) {
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].status !== "intable") {
+			io.to(players[i].id).emit(event, message);
 		}
 	}
 };
 
-Messaging.prototype.sendEventToAllPlayersButPlayer = function(event,message,io,players,player) {
-	for(var i = 0; i < players.length; i++) {
-		if(players[i].id != player.id) {
+Messaging.prototype.sendEventToAllPlayersButPlayer = function (event, message, io, players, player) {
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].id != player.id) {
 			io.to(players[i].id).emit(event, message);
 		}
-	}	
+	}
 };
-Messaging.prototype.sendEventToAllPlayersButZhuangOrXian = function(event,message,io,players,zhuang,xian) {
-	for(var i = 0; i < players.length; i++) {
-		if(players[i].id != zhuang.id&&players[i].id !=xian.id ) {
+Messaging.prototype.sendEventToAllPlayersButZhuangOrXian = function (event, message, io, players, zhuang, xian) {
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].id != zhuang.id && players[i].id != xian.id) {
 			io.to(players[i].id).emit(event, message);
 		}
-	}	
-};
-
-Messaging.prototype.sendEventToAPlayer = function(event,message,io,players,player) {
-	for(var i = 0; i < players.length; i++) {
-		if(players[i].id == player.id) {
-			io.to(players[i].id).emit(event, message);
-		}
-	}	
+	}
 };
 
-Messaging.prototype.sendEventToZhuangJia = function(event,io,players,player) {
-	for(var i = 0; i < players.length; i++) {
-		if(players[i].id == player.id) {
+Messaging.prototype.sendEventToAPlayer = function (event, message, io, players, player) {
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].id == player.id) {
+			io.to(players[i].id).emit(event, message);
+		}
+	}
+};
+
+Messaging.prototype.sendEventToZhuangJia = function (event, io, players, player) {
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].id == player.id) {
 			io.to(players[i].id).emit(event);
 		}
-	}	
+	}
 };
 
 
